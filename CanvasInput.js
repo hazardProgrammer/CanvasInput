7c303f489001c26e8d5ddca9449969821f1a210e
hazardProgrammer <mlg.allen01@gmail.com>
2017-11-22 22:51:19 -0500

    Update CanvasInput.js

--- CanvasInput.js
+++ CanvasInput.js
@@ -641,49 +641,7 @@
      * @param  {Boolean} doReturn (optional) True to prevent a premature render.
      * @return {Mixed}          CanvasInput or current box shadow.
      */
-    boxShadow: function(data, doReturn) {
-      var self = this;
-
-      if (typeof data !== 'undefined') {
-        // parse box shadow
-        var boxShadow = data.split('px ');
-        self._boxShadow = {
-          x: self._boxShadow === 'none' ? 0 : parseInt(boxShadow[0], 10),
-          y: self._boxShadow === 'none' ? 0 : parseInt(boxShadow[1], 10),
-          blur: self._boxShadow === 'none' ? 0 : parseInt(boxShadow[2], 10),
-          color: self._boxShadow === 'none' ? '' : boxShadow[3]
-        };
-
-        // take into account the shadow and its direction
-        if (self._boxShadow.x < 0) {
-          self.shadowL = Math.abs(self._boxShadow.x) + self._boxShadow.blur;
-          self.shadowR = self._boxShadow.blur + self._boxShadow.x;
-        } else {
-          self.shadowL = Math.abs(self._boxShadow.blur - self._boxShadow.x);
-          self.shadowR = self._boxShadow.blur + self._boxShadow.x;
-        }
-        if (self._boxShadow.y < 0) {
-          self.shadowT = Math.abs(self._boxShadow.y) + self._boxShadow.blur;
-          self.shadowB = self._boxShadow.blur + self._boxShadow.y;
-        } else {
-          self.shadowT = Math.abs(self._boxShadow.blur - self._boxShadow.y);
-          self.shadowB = self._boxShadow.blur + self._boxShadow.y;
-        }
-
-        self.shadowW = self.shadowL + self.shadowR;
-        self.shadowH = self.shadowT + self.shadowB;
-
-        self._calcWH();
-
-        if (!doReturn) {
-          self._updateCanvasWH();
-
-          return self.render();
-        }
-      } else {
-        return self._boxShadow;
-      }
-    },
+    
 
     /**
      * Get/set the inner shadow.
